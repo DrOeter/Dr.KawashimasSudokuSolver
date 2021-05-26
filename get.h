3a1c8e8551e41fe24fb613e5a56c5066aa233497
Moritz Mögel <80258649+DrOeter@users.noreply.github.com>
2021-05-26 17:18:17 +0200

    Update get.h

--- get.h
+++ get.h
@@ -7,6 +7,128 @@
 void MainWindow::getGui(){
     std::vector<unsigned short> line;
     line.push_back( ui->a1->text().toInt() );
+    line.push_back( 9 );//ui->a2->text().toInt() );
+    line.push_back( 3 );//ui->a3->text().toInt() );
+    line.push_back( 4 );//ui->a4->text().toInt() );
+    line.push_back( ui->a5->text().toInt() );
+    line.push_back( ui->a6->text().toInt() );
+    line.push_back( 8 );//ui->a7->text().toInt() );
+    line.push_back( ui->a8->text().toInt() );
+    line.push_back( ui->a9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( ui->b1->text().toInt() );
+    line.push_back( ui->b2->text().toInt() );
+    line.push_back( 7 );//ui->b3->text().toInt() );
+    line.push_back( ui->b4->text().toInt() );
+    line.push_back( ui->b5->text().toInt() );
+    line.push_back( ui->b6->text().toInt() );
+    line.push_back( ui->b7->text().toInt() );
+    line.push_back( ui->b8->text().toInt() );
+    line.push_back( 3 );//ui->b9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( ui->c1->text().toInt() );
+    line.push_back( ui->c2->text().toInt() );
+    line.push_back( ui->c3->text().toInt() );
+    line.push_back( ui->c4->text().toInt() );
+    line.push_back( 2 );//ui->c5->text().toInt() );
+    line.push_back( ui->c6->text().toInt() );
+    line.push_back( ui->c7->text().toInt() );
+    line.push_back( 4 );//ui->c8->text().toInt() );
+    line.push_back( ui->c9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( ui->d1->text().toInt() );
+    line.push_back( 6 );//ui->d2->text().toInt() );
+    line.push_back( ui->d3->text().toInt() );
+    line.push_back( 8 );//ui->d4->text().toInt() );
+    line.push_back( ui->d5->text().toInt() );
+    line.push_back( 2 );//ui->d6->text().toInt() );
+    line.push_back( ui->d7->text().toInt() );
+    line.push_back( ui->d8->text().toInt() );
+    line.push_back( ui->d9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( 9 );//ui->e1->text().toInt() );
+    line.push_back( ui->e2->text().toInt() );
+    line.push_back( ui->e3->text().toInt() );
+    line.push_back( ui->e4->text().toInt() );
+    line.push_back( ui->e5->text().toInt() );
+    line.push_back( ui->e6->text().toInt() );
+    line.push_back( ui->e7->text().toInt() );
+    line.push_back( ui->e8->text().toInt() );
+    line.push_back( 4 );//ui->e9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( ui->f1->text().toInt() );
+    line.push_back( ui->f2->text().toInt() );
+    line.push_back( ui->f3->text().toInt() );
+    line.push_back( 6 );//ui->f4->text().toInt() );
+    line.push_back( ui->f5->text().toInt() );
+    line.push_back( 1 );//ui->f6->text().toInt() );
+    line.push_back( ui->f7->text().toInt() );
+    line.push_back( 7 );//ui->f8->text().toInt() );
+    line.push_back( ui->f9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( ui->g1->text().toInt() );
+    line.push_back( 7 );//ui->g2->text().toInt() );
+    line.push_back( ui->g3->text().toInt() );
+    line.push_back( ui->g4->text().toInt() );
+    line.push_back( 6 );//ui->g5->text().toInt() );
+    line.push_back( ui->g6->text().toInt() );
+    line.push_back( ui->g7->text().toInt() );
+    line.push_back( ui->g8->text().toInt() );
+    line.push_back( ui->g9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( 1 );//ui->h1->text().toInt() );
+    line.push_back( ui->h2->text().toInt() );
+    line.push_back( ui->h3->text().toInt() );
+    line.push_back( ui->h4->text().toInt() );
+    line.push_back( ui->h5->text().toInt() );
+    line.push_back( ui->h6->text().toInt() );
+    line.push_back( 9 );//ui->h7->text().toInt() );
+    line.push_back( ui->h8->text().toInt() );
+    line.push_back( ui->h9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+    line.push_back( ui->i1->text().toInt() );
+    line.push_back( ui->i2->text().toInt() );
+    line.push_back( 6 );//ui->i3->text().toInt() );
+    line.push_back( ui->i4->text().toInt() );
+    line.push_back( ui->i5->text().toInt() );
+    line.push_back( 8 );//ui->i6->text().toInt() );
+    line.push_back( 2 );//ui->i7->text().toInt() );
+    line.push_back( 3 );//ui->i8->text().toInt() );
+    line.push_back( ui->i9->text().toInt() );
+
+    field.push_back(line);
+    line.clear();
+
+}
+
+/*
+void MainWindow::getGui(){
+    std::vector<unsigned short> line;
+    line.push_back( ui->a1->text().toInt() );
     line.push_back( ui->a2->text().toInt() );
     line.push_back( ui->a3->text().toInt() );
     line.push_back( ui->a4->text().toInt() );
@@ -123,7 +245,7 @@ void MainWindow::getGui(){
     field.push_back(line);
     line.clear();
 
-}
+}*/
 /*
 void MainWindow::getGui(){
     std::vector<unsigned short> line;
@@ -660,9 +782,9 @@ void MainWindow::getGui(){
     field.push_back(line);
     line.clear();
 }
+*/
 
-*//*
-
+/*
 void MainWindow::getGui(){
     std::vector<unsigned short> line;
     line.push_back( ui->a1->text().toInt() );
