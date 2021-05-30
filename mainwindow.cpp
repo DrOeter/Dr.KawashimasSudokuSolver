52724ffa9dd146ba3cac1d2954db1d062a400480
Moritz Mögel <80258649+DrOeter@users.noreply.github.com>
2021-05-30 23:09:32 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,13 +20,50 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     on_button_clicked();
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+bool MainWindow::eventFilter(QObject* object, QEvent* event){
+    uint16_t find = 0;
+    QLineEdit *line = NULL;
+
+    for(auto i: clues)
+        if(object == i){
+            line = i;
+            find++;
+        }
+
+    if( find == 1 && event->type() == QEvent::KeyPress) {
+        line->setStyleSheet("QLineEdit { background-color: #1f1f1f } ");
+        line->setText("");
+        return false;
+    }
+    if( find == 1 && event->type() == QEvent::FocusOut) {
+        char c = line->text().toStdString().c_str()[0];
+
+        if( line->text().isEmpty() ) line->setStyleSheet("QLineEdit { background-color: #303030 } ");
+        if( !line->text().isEmpty() && ( c < 48 || c > 57) ) {
+            line->setStyleSheet("QLineEdit { background-color: #303030 } ");
+            line->clear();
+        }
+
+
+        return false;
+    }
+    return false;
 }
 
-void MainWindow::on_button_clicked()
-{
+MainWindow::~MainWindow(){
+    delete ui;
+    delete sudoku;
+}
+
+void MainWindow::on_button_clicked(){
     getGui();
+    uint16_t it = 0;
+
+    for(auto &i: field)
+        for(auto ii: i){
+            if( ii != 0 ) clues[it]->setStyleSheet("QLineEdit { background-color: #1f1f1f } ");
+            it++;
+        }
+
     sudoku = new Sudoku(field, clues, pencil);
 }
